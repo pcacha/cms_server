c4a24be49d823660575d707de5f308d8fbecacfa
pcacha <pavelcacha@email.cz>
2021-07-19 21:43:28 +0200

    first commit

---
+++ src/main/java/cz/zcu/students/cacha/cms_server/CmsServerApplication.java
@@ -0,0 +1,13 @@
+package cz.zcu.students.cacha.cms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/zcu/students/cacha/cms_server/CmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zcu.students.cacha.cms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
